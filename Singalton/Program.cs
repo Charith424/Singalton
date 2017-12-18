6882fe846eb631f59895f0f076d688c23b9afc25
chari <chari@DESKTOP-DR8C3OD>
2017-12-18 07:24:41 +0530

    Add project files.

---
+++ Singalton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singalton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Singolton.callsingleton();
+            Console.ReadLine();
+        }
+    }
+
+    class Singolton {
+        private Singolton() {
+
+        }
+
+        private static Singolton singleobj =new Singolton();
+
+        public static void callsingleton() {
+            Console.WriteLine("I'm singolton");
+
+        }
+    }
+}
